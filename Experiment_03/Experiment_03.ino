53f8be7106961792d40c557cb50a9e60bba1213d
Maryam Tariq <martariq24@gmail.com>
2026-05-24 16:20:13 +0500

    Experiment_03.ino

--- Experiment_03/Experiment_03.ino
+++ Experiment_03/Experiment_03.ino
@@ -1,14 +1,14 @@
-int potPin = A0; // Potentiometer ki middle pin A0 par
+int potPin = A0; // Potentiometer A0 pin in middle
 
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  int sensorValue = analogRead(potPin); // 0 se 1023 tak value milegi
+  int sensorValue = analogRead(potPin); // You will get values in between 0 to 1023 
   
   Serial.print("Analog Value: ");
   Serial.println(sensorValue);
   
-  delay(100); // Thoda ruk kar read karein
+  delay(100); // Wait
 }
