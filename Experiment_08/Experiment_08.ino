77337995ad11adb2cdcb2d69ebf21f94f8bcf36e
Maryam Tariq <martariq24@gmail.com>
2026-05-24 19:16:58 +0500

    Experiment_08.ino

--- Experiment_08/Experiment_08.ino
+++ Experiment_08/Experiment_08.ino
@@ -1,9 +1,8 @@
 void setup() {
-  pinMode(2, INPUT_PULLUP); // Internal resistor use kar rahe hain
+  pinMode(2, INPUT_PULLUP); 
   Serial.begin(9600);
 }
 void loop() {
-  // PULLUP mode mein: button dabane par 0, chhorne par 1 aata hai
   Serial.println(digitalRead(2)); 
   delay(200);
 }
